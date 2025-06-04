6f697d76547dfc057bb9b588d2f7dcb61f4a2fbe
Likssss <148707565+Likssss@users.noreply.github.com>
2025-06-04 18:13:55 +0700

    fixed minor issues

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,9 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
-      {/* <Route path="/products" element={<ProductPage />} />
-      <Route path="/contact" element={<ContactUsPage />} />
-      <Route path="/projects" element={<ProjectPage />} /> */}
+      {/* <Route path="/products" element={<ProductPage />} /> */}
+      {/* <Route path="/contact" element={<ContactUsPage />} /> */}
+      {/* <Route path="/projects" element={<ProjectPage />} /> */}
       <Route path="/process" element={<ProcessPage />} />
       <Route path="/about" element={<AboutPage />} />
     </Routes>
